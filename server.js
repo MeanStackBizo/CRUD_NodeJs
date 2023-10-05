eda50935e5d56cfb6247b144cc529d8d7147d9dd
TalelMejri <talelmejri8@gmail.com>
2023-10-05 06:58:40 +0100

    setup to send a response

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http=require("http")
+const app=require("./app")
 
-const server=http.createServer((req,res)=>{
-    res.end("Hello talel")
-})
+app.set('port',process.env.Port || 3000)
 
+// const server=http.createServer((req,res)=>{
+//     res.end("Hello talel")
+// })
+const server=http.createServer(app);
 
 server.listen(3000);
