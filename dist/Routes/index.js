d1bbb4834206c39e4cfcd735ba49c834b489eff1
TalelMejri <talelmejri8@gmail.com>
2023-10-07 19:05:18 +0100

    router

--- dist/Routes/index.js
+++ dist/Routes/index.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const Isauth_1 = require("../middlewars/Isauth");
+const ProfileController_1 = require("../controller/ProfileController");
 const router = express_1.default.Router();
 router.use(Isauth_1.fetchUser);
 require("./booksRoutes")(router);
 require("./UserRoutes")(router);
+router.get("/profile", ProfileController_1.profileUser);
 exports.default = router;
--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {fetchUser} from "../middlewars/Isauth"
+import {profileUser} from "../controller/ProfileController"
 const router=express.Router();
 
 router.use(fetchUser);
 
 require("./booksRoutes")(router);
 require("./UserRoutes")(router);
+// router.get("/profile",profileUser)
 
 export default router;
