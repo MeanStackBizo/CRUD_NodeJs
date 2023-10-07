0bb5ffdc4960c71ff886c0f5f2c1bde54ed5cd36
TalelMejri <talelmejri8@gmail.com>
2023-10-07 19:05:10 +0100

    middelware isAuth

--- dist/middlewars/Isauth.js
+++ dist/middlewars/Isauth.js
@@ -4,25 +4,14 @@ exports.fetchUser = void 0;
 const jwt = require('jsonwebtoken');
 const fetchUser = (req, res, next) => {
     var _a;
-    // res.status(401).json({error: req.path});
-    //});
     if (req.path.includes("user")) {
         next();
     }
     else {
         try {
-            const token = (_a = req.header("Authorization")) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
-            const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-            const userId = decodedToken.userId;
-            if (!req.headers.userid) {
-                throw 'Bad userID request';
-            }
-            else if (req.headers.userid !== userId) {
-                throw 'Invalid user ID';
-            }
-            else {
-                next();
-            }
+            const token = (_a = req.header("authorization")) === null || _a === void 0 ? void 0 : _a.substring(7);
+            const decodedToken = jwt.verify(token, 'Token_Secret');
+            next();
         }
         catch (error) {
             res.status(401).json({
--- src/middlewars/Isauth.ts
+++ src/middlewars/Isauth.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-
 const jwt = require('jsonwebtoken');
 
 export const fetchUser  =  (req:Request, res:Response, next:NextFunction) => {
@@ -7,16 +6,9 @@ export const fetchUser  =  (req:Request, res:Response, next:NextFunction) => {
          next();
     }else{
         try {
-            const token = req.header("Authorization")?.split(' ')[1];
-            const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-            const userId = decodedToken.userId;
-            if (!req.headers.userid) {
-                throw 'Bad userID request';
-            } else if (req.headers.userid !== userId) {
-              throw 'Invalid user ID';
-            } else {
-              next();
-            }
+            const token = req.header("authorization")?.substring(7);
+            const decodedToken = jwt.verify(token, 'Token_Secret');
+            next();
           } catch (error:any) {
             res.status(401).json({
               error: error.message
