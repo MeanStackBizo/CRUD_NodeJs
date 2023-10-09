befc7f8f08ddf4b19c7f8b31904ea2ee0f21cd81
TalelMejri <talelmejri8@gmail.com>
2023-10-10 00:25:16 +0100

    Navigate and MessageError

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {User} from "../../Model/User"
 import { AuthServiceService } from 'src/app/Service/auth-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  ngOnInit(): void {
+      this.ActivatedRoute.queryParams.subscribe((res:any)=>{
+         this.messageError=res['message']
+      })
+      setTimeout(()=>{
+        this.messageError="";
+        this.Router.navigate([''])
+      },3000);
+  }
   user:User=new User();
-  constructor(private AuthServiceService:AuthServiceService){
+  constructor(private AuthServiceService:AuthServiceService,private Router:Router,private ActivatedRoute:ActivatedRoute){
   }
   
   messageError:String=""
   login(){
       this.AuthServiceService.login({email:this.user.email,password:this.user.password}).subscribe((res:any)=>{
         console.log(res);
+        this.Router.navigate(['/profile'])
       },(error:any)=>{
         console.log(error);
         this.messageError=error.error.message;
