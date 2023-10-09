8099a4b180c2d56c49c6098c7c7a27fb7deaa319
TalelMejri <talelmejri8@gmail.com>
2023-10-10 00:23:40 +0100

    Fix IsQuard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ProfileComponent } from './profile/profile.component';
+import { IsAuthGuard } from './guard/is-auth.guard';
 const routes: Routes = [
   {path:'',redirectTo:'auth',pathMatch:'full'},
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {path:"profile",component:ProfileComponent,canActivate:[IsAuthGuard]}
 ];
 
 @NgModule({
