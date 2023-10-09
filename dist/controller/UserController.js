5da164c8e039fa56336b3fcc141aaed9194a43c1
TalelMejri <talelmejri8@gmail.com>
2023-10-09 07:10:57 +0100

    return user

--- dist/controller/UserController.js
+++ dist/controller/UserController.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const user_1 = __importDefault(require("../model/user"));
+const user_2 = __importDefault(require("../model/user"));
 exports.signup = (req, res) => {
     bcrypt
         .hash(req.body.password, 10)
@@ -37,23 +29,23 @@ exports.signup = (req, res) => {
         res.status(500).json({ message: error });
     });
 };
-exports.login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    user_1.default.findOne({ email: req.body.email }).then((user) => {
+exports.login = (req, res) => {
+    user_2.default.findOne({ email: req.body.email }).then((user) => {
         if (!user) {
-            res.status(401).json({ error: 'User Not Found' });
+            res.status(404).json({ "message": "User Not Found" });
         }
         bcrypt.compare(req.body.password, user.password).then((valid) => {
             if (!valid) {
-                res.status(401).json({ error: "Wrong Password" });
+                res.status(404).json({ "message": "Password Ghaleeeet 😒" });
             }
             res.status(200).json({
-                user: user,
-                token: jwt.sign({ user: user }, 'Token_Secret')
+                token: jwt.sign({ user: user }, "TokenSecret"),
+                user: user
             });
         }).catch((error) => {
-            res.status(500).json({ error });
+            res.status(500).json({ "message": error });
         });
     }).catch((error) => {
-        res.status(500).json({ error });
+        res.status(500).json({ "message": error });
     });
-});
+};
