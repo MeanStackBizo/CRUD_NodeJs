f8992ce129fb7acf8ef7bc95121c2f32af7dc138
TalelMejri <talelmejri8@gmail.com>
2023-10-09 07:11:09 +0100

    LoginComp

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {User} from "../../Model/User"
+import { AuthServiceService } from 'src/app/Service/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  user:User=new User();
+  constructor(private AuthServiceService:AuthServiceService){
+  }
+  
+  messageError:String=""
+  login(){
+      this.AuthServiceService.login({email:this.user.email,password:this.user.password}).subscribe((res:any)=>{
+        console.log(res);
+      },(error:any)=>{
+        console.log(error);
+        this.messageError=error.error.message;
+      })
+     
+  }
 }
