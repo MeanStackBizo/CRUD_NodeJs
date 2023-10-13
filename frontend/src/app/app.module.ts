f1c0623d0786081fe1fa171466038983ff7e68a4
TalelMejri <talelmejri8@gmail.com>
2023-10-13 17:27:25 +0100

    Compoenent books

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,21 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { InterceptorService } from './Service/interceptor.service';
 import { ProfileComponent } from './profile/profile.component';
 import { IsAuthGuard } from './guard/is-auth.guard';
+import { ConsultebookComponent } from './Books/consultebook/consultebook.component';
+import { AddBookComponent } from './Books/add-book/add-book.component';
+import { InfoBookComponent } from './Books/info-book/info-book.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    ConsultebookComponent,
+    AddBookComponent,
+    InfoBookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
     
   ],
   providers: [
