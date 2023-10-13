fb2fff3d4987eafdbef02bb6a1cc1103a669932e
TalelMejri <talelmejri8@gmail.com>
2023-10-13 17:28:08 +0100

    ProfileComponent

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthServiceService } from '../Service/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +9,7 @@ import { AuthServiceService } from '../Service/auth-service.service';
 })
 export class ProfileComponent {
   
-    constructor(private AuthServiceService:AuthServiceService){
+    constructor(private AuthServiceService:AuthServiceService,private Router:Router){
         this.getUser();
     }
     User:any;
@@ -16,6 +17,11 @@ export class ProfileComponent {
       this.AuthServiceService.getUser().subscribe((res:any)=>{
            this.User=res?.message[0];
       })
+    }
+
+    logout(){
+      localStorage.clear();
+      this.Router.navigate([''])
     }
 
 }
